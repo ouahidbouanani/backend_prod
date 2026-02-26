469c5696a800cd2fcd156d29bf34010eda56a758
Ouahid BOUANANI <ouahidbouanani@treefrog.lan>
2026-02-26 11:12:34 +0100

    fix bug forms fini

--- controllers/admin/semiFinisAchetesController.ts
+++ controllers/admin/semiFinisAchetesController.ts
@@ -1,6 +1,8 @@
 // src/controllers/semiFinisAchetesController.ts
 import { Request, Response, NextFunction } from 'express';
-import prisma from '../../config/prisma';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export const getSemiFinisAchetes = async (req: Request, res: Response, next: NextFunction) => {
   try {
